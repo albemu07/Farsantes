2d61773d8f5d31f858687e160b1e8c0017b4c24c
albemu07 <albemu07@ucm.es>
2020-12-10 14:05:25 +0100

    error arreglado

--- Scripts/Box.js
+++ Scripts/Box.js
@@ -1,8 +1,8 @@
 import Objeto from './objeto.js'; 
 export default class Box extends Objeto{
     constructor(scene, x, y,sprite){
+        super(scene, x, y, sprite, 500, 500, 0.20, true); 
         this.radio = 55;
-        super(scene, x, y, sprite, 500, 500, 0.20, true);        
         this.cursors = this.scene.input.keyboard.addKeys({
             grab: Phaser.Input.Keyboard.KeyCodes.C,
             grabb: Phaser.Input.Keyboard.KeyCodes.M
