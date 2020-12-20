32aa114729fd096630a89647dc591cc40a7f6ef1
albemu07 <albemu07@ucm.es>
2020-12-20 17:32:58 +0100

    import changes del boot

--- scripts/bootScene.js
+++ scripts/bootScene.js
@@ -5,48 +5,48 @@ export default class Boot extends Phaser.Scene{
 
     preload(){
         //Carga de todos los recursos
-        this.load.spritesheet('IdleBuffoon','./Assets/Buffoon/idle.png',{frameWidth:150,frameHeight:150});
-        this.load.spritesheet('RunBuffoon','./Assets/Buffoon/run.png',{frameWidth:150,frameHeight:150});
-        this.load.spritesheet('IdleCountess','./Assets/Countess/idle.png',{frameWidth:320,frameHeight:320});
-        this.load.spritesheet('RunCountess','./Assets/Countess/run.png',{frameWidth:320,frameHeight:320});
-        this.load.image('guardiaL','./Assets/Guard/guardiaLeft.png');
-        this.load.image('guardiaR','./Assets/Guard/guardiaRight.png');
-        this.load.image('guardiaF','./Assets/Guard/guardiaFront.png');
-        this.load.image('guardiaB','./Assets/Guard/guardiaBack.png');
-        this.load.image('monjeL','./Assets/Nun/monjeLeft.png');
-        this.load.image('monjeR','./Assets/Nun/monjeRight.png');
-        this.load.image('monjeF','./Assets/Nun/monjeFront.png');
-        this.load.image('monjeB','./Assets/Nun/monjeBack.png');
-        this.load.image('dialogo','./Assets/Guard/dialogo.png')
-        this.load.image('ring', '/Assets/ring/ring.png');
-        this.load.image('mud', '/Assets/Mud/mod.png');
-        this.load.image('map', './Assets/Tile/SpriteSetBien.png');
+        this.load.spritesheet('IdleBuffoon','./assets/buffoon/idle.png',{frameWidth:150,frameHeight:150});
+        this.load.spritesheet('RunBuffoon','./assets/buffoon/run.png',{frameWidth:150,frameHeight:150});
+        this.load.spritesheet('IdleCountess','./assets/countess/idle.png',{frameWidth:320,frameHeight:320});
+        this.load.spritesheet('RunCountess','./assets/countess/run.png',{frameWidth:320,frameHeight:320});
+        this.load.image('guardiaL','./assets/guard/guardiaLeft.png');
+        this.load.image('guardiaR','./assets/guard/guardiaRight.png');
+        this.load.image('guardiaF','./assets/guard/guardiaFront.png');
+        this.load.image('guardiaB','./assets/guard/guardiaBack.png');
+        this.load.image('monjeL','./assets/nun/monjeLeft.png');
+        this.load.image('monjeR','./assets/nun/monjeRight.png');
+        this.load.image('monjeF','./assets/nun/monjeFront.png');
+        this.load.image('monjeB','./assets/nun/monjeBack.png');
+        this.load.image('dialogo','./assets/guard/dialogo.png')
+        this.load.image('ring', '/assets/ring/ring.png');
+        this.load.image('mud', '/assets/mud/mod.png');
+        this.load.image('map', './assets/tile/spriteSetBien.png');
     
-        this.load.image('vista','./Assets/SpriteSheet/deteccion.png') ;
-        this.load.image('BoxSprite', './Assets/Box/box.png');
-        this.load.image('Trigger','./Assets/Mechanisms/nextZoneTrigger.png');
-        this.load.spritesheet('Lever1','./Assets/Lever/lever1.png',{frameWidth:32,frameHeight:32});
-        this.load.spritesheet('Lever2','./Assets/Lever/lever2.png',{frameWidth:32,frameHeight:32});
-        this.load.image('LeverOpen','./Assets/Lever/Lever_Open.png');
-        this.load.image('LeverClose','./Assets/Lever/Lever_Close.png');
-        this.load.spritesheet('DoorOpen','./Assets/Door/door_Open.png',{frameWidth:920,frameHeight:1127});
-        this.load.spritesheet('DoorClose','./Assets/Door/door_Close.png',{frameWidth:820,frameHeight:1035});
-        this.load.tilemapTiledJSON('tilemap', './Assets/Tile/nivelPrueba.json');
+        this.load.image('vista','./assets/spriteSheet/deteccion.png') ;
+        this.load.image('BoxSprite', './assets/box/box.png');
+        this.load.image('Trigger','./assets/mechanisms/nextZoneTrigger.png');
+        this.load.spritesheet('Lever1','./assets/lever/lever1.png',{frameWidth:32,frameHeight:32});
+        this.load.spritesheet('Lever2','./assets/lever/lever2.png',{frameWidth:32,frameHeight:32});
+        this.load.image('LeverOpen','./assets/lever/leverOpen.png');
+        this.load.image('LeverClose','./assets/lever/leverClose.png');
+        this.load.spritesheet('DoorOpen','./assets/door/doorOpen.png',{frameWidth:920,frameHeight:1127});
+        this.load.spritesheet('DoorClose','./assets/door/doorClosed.png',{frameWidth:820,frameHeight:1035});
+        this.load.tilemapTiledJSON('tilemap', './assets/tile/nivelPrueba.json');
 
         //Placa de presión
-        this.load.image('plateUnpressed','./Assets/PresurePlate/plateUnpressed.png')
-        this.load.image('platePressed','./Assets/PresurePlate/platePressed.png')
+        this.load.image('plateUnpressed','./assets/presurePlate/plateUnpressed.png')
+        this.load.image('platePressed','./assets/presurePlate/platePressed.png')
 
 
         //Assets del menú
-        this.load.image('menuBackground','./Assets/Menu/menuBackground1.png')
-        this.load.image('settingsBackground','./Assets/Menu/menuBackground2.png')
-        this.load.image('pauseBackground','./Assets/Menu/menuBackground3.png')
-        this.load.image('playButton','./Assets/Menu/PlayButton.png')
-        this.load.image('settingsButton','./Assets/Menu/SettingsButton.png')
-        this.load.image('backButton','./Assets/Menu/BackButton.png')
-        this.load.image('exitButton','./Assets/Menu/exitButton.png')
-        this.load.image('gameButton','./Assets/Menu/gameButton.png')
+        this.load.image('menuBackground','./assets/menu/menuBackground1.png')
+        this.load.image('settingsBackground','./assets/menu/menuBackground2.png')
+        this.load.image('pauseBackground','./assets/menu/menuBackground3.png')
+        this.load.image('playButton','./assets/menu/playButton.png')
+        this.load.image('settingsButton','./assets/menu/settingsButton.png')
+        this.load.image('backButton','./assets/menu/backButton.png')
+        this.load.image('exitButton','./assets/menu/exitButton.png')
+        this.load.image('gameButton','./assets/menu/gameButton.png')
 
         
 
