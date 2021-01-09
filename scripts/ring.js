ceb62d8879a332c4267cd65fd85e0e908239b73e
liyliLee <liyli@ucm.es>
2021-01-09 13:43:32 +0100

    tilemap3 funciona

--- scripts/ring.js
+++ scripts/ring.js
@@ -1,6 +1,6 @@
 import Objeto from './objeto.js';
 export default class Ring extends Phaser.GameObjects.Container {
-    constructor(scene, x, y, sprite,sprite2,value,angle){
+    constructor(scene, x, y,value,angle){
         super(scene, x, y);
         scene.add.existing(this);
         scene.physics.world.enable(this);
@@ -9,14 +9,35 @@ export default class Ring extends Phaser.GameObjects.Container {
         this.body.setSize(100,100);
         this.body.immovable = true;
         this.setAngle(angle);
-        this.sprite=sprite;
-        this.sprite2=sprite2;
         this.scene= scene;
-        this.ring = new Objeto(scene, 50, 50, sprite, 32, 32, 1, false);
+        this.checkAnim();
+        this.ring = new Objeto(scene, 50, 50, this.sprite, 32, 32, 1, false);
         this.add(this.ring);
         
     }
-
+    checkAnim()
+    {
+        if (this.value ===100)
+        {
+            this.sprite='ringRC';
+            this.sprite2='ringR';
+        }        
+        else if (this.value ===200)
+        {
+            this.sprite='ringGC';
+            this.sprite2='ringG';
+        }        
+        else if (this.value ===300)
+        {
+            this.sprite='ringBC';
+            this.sprite2='ringB';
+        }        
+        else if (this.value ===400)
+        {
+            this.sprite='ringYC';
+            this.sprite2='ringY';
+        }        
+    }
     animate()
     {
         this.ring.setTexture(this.sprite2);
---
+++ scripts/zone3.js
@@ -0,0 +1,7 @@
+import Game from '../game.js'
+
+export default class Zone3 extends Game {
+  constructor() {
+    super('Zone3','Zone1',48,320,48,352, 'tilemap1');
+  }    
+}
