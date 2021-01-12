ec68d36b57cb901c9930bbdb95fec839dcd06817
albemu07 <albemu07@ucm.es>
2021-01-12 13:25:55 +0100

    puertsa  cerradas

--- scripts/menuScene.js
+++ scripts/menuScene.js
@@ -13,22 +13,22 @@ export default class Menu extends Phaser.Scene{
             loop: true,
             delay:0
         }
-        this.music = this.sound.add("music", config)
+        this.music = this.sound.add("music", config);
 
         //Fondo
-        this.add.image(0,0,'menuBackground').setOrigin(0,0)
+        this.add.image(0,0,'menuBackground').setOrigin(0,0);
 
         //Botones
-        this.play=this.add.image(443,320,'playButton').setScale(1.2)
-        this.settings=this.add.image(443,480,'settingsButton').setScale(1.2)
+        this.play=this.add.image(443,320,'playButton').setScale(1.2);
+        this.settings=this.add.image(443,480,'settingsButton').setScale(1.2);
 
         //Interacciones
         this.play.setInteractive();
         this.settings.setInteractive();
 
         //Cambio de tamaño al pasar el ratón por encima
-        this.play.on('pointerover',()=>{this.play.setScale(1.4)})
-        this.play.on('pointerout',()=>{this.play.setScale(1.2)})
+        this.play.on('pointerover',()=>{this.play.setScale(1.4)});
+        this.play.on('pointerout',()=>{this.play.setScale(1.2)});
 
         this.settings.on('pointerover',()=>{this.settings.setScale(1.4)})
         this.settings.on('pointerout',()=>{this.settings.setScale(1.2)})
