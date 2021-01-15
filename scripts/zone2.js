549d7db045d46f3d8857a08a9cc287e1679e9fc4
oscar <osgarc05@ucm.es>
2021-01-15 18:44:40 +0100

    Ultimo retoque

--- scripts/zone2.js
+++ scripts/zone2.js
@@ -1,7 +1,7 @@
 import Game from '../game.js'
 export default class Zone2 extends Game {
     constructor() {
-      super('Zone2','Zone3',32,320,32,352,'tilemap12');
+      super('Zone2','Zone3',32,320,32,352,'tilemap12', 1);
     }
   
 }
